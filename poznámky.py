20c0ab23a7f9913dd6f9d2760348dc53469ddd1a
unknown <kristyna.savelova@infis.cz>
2025-03-11 09:28:38 +0100

    text

---
+++ poznámky.py
@@ -0,0 +1,39 @@
+#šavelová
+
+#načtení od uživatele:
+barva = str(input("zadejte vaší oblíbenou barvu:"))
+
+#příklad přetypování na číslo:
+cislo = int(input())
+#příklad přetypování naboolean:
+pravdaNepravda = bool(input())
+
+#podmínky:
+if cislo>0:
+    print("číslo je kladné")
+elif cislo<0:
+    print("číslo je záporné")
+else:
+    print("zadejte jiné číslo")
+
+#logické operátory:
+x = int(input("zadejte číslo: "))
+y = int(input("zadejte druhé číslo: "))
+if x>3 and y<20:
+    print()
+elif x>3 or y<20:
+    print()
+elif not (x == 3):
+    print()
+else:
+    print()
+
+#github příkaz 1:
+#git add *
+
+#github příkaz 2:
+#git commit -m "text"
+
+#github příkaz 3:
+#git push
+
